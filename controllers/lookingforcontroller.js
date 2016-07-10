feb8b5b9eda414d863f48b11359e06af199cd06d
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-07-10 15:19:44 -0400

    pulling

--- controllers/lookingforcontroller.js
+++ controllers/lookingforcontroller.js
@@ -1,7 +1,5 @@
 module.exports = function(app) {
     app.controller('LookingForController', ['$scope', '$http', function($scope, $http) {
-        $scope.loginClick = function() {
-            console.log('clicked')
-        }
+      $scope.musician = "";
     }]);
 };
